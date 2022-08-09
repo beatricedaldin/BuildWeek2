a69ac8afa4d6bfaa9535dc3446dd2da712aef1cc
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-08-09 16:51:28 +0200

    modfdgd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,97 @@
-import {User, Book, apiUrlB, apiUrlU} from './class.js'
+import { User, Book, apiUrlB, apiUrlU } from './class.js'
+
 
 
 fetch(apiUrlU)
-.then(res => res.json())
-.then(users => {
-    let userSection = document.querySelector('#users')
+    .then(res => res.json())
+    .then(users => {
+        let userSection = document.querySelector('#users')
 
-    for(let user of users){
-        user = new User(user.name, user.surname, user.dateOfBirth, user.region, user.rank, user.id)
-        let userName = document.createElement('a')
-        userName.innerHTML = user.name + '' + user.surname
-        userSection.append(userName)
-        userName.href = 'user.html?id=' + user.id
+        for (let user of users) {
+            user = new User(user.name, user.surname, user.dateOfBirth, user.region, user.rank, user.id)
+            let userName = document.createElement('a')
+            userName.innerHTML = user.name + '' + user.surname
+            userSection.append(userName)
+            userName.href = 'user.html?id=' + user.id
 
-        if(user.rank == 'Elementary'){
-            userName.style.color = 'green'
-        } else if(user.rank == 'Intermediate'){
-            userName.style.color = 'blue'
-        } else if (user.rank == 'Advanced'){
-            userName.style.color = 'red'
+            if (user.rank == 'Elementary') {
+                userName.style.color = 'green'
+            } else if (user.rank == 'Intermediate') {
+                userName.style.color = 'blue'
+            } else if (user.rank == 'Advanced') {
+                userName.style.color = 'red'
+            }
+
         }
-        
-    }
 
-    
 
-})
+
+    })
 
 
 fetch(apiUrlB)
-.then(res => res.json())
-.then(books => {
-    let bookSection = document.querySelector('#books')
+    .then(res => res.json())
+    .then(books => {
+        let bookSection = document.querySelector('#books')
 
-    for(let book of books){
-    book = new Book(book.title, book.author, book.year, book.review, book.sharedBy, book.availability, book.id)
-    let bookTitle = document.createElement('a')
-    bookTitle.innerHTML = book.title
-    bookSection.append(bookTitle)
-    bookTitle.addEventListener('click', function(){
-        let titleB = document.querySelector('#titleB')
-        titleB.innerHTML = book.title
-        let authorB = document.querySelector('#authorB')
-        authorB.innerHTML = book.author
-        let yearB = document.querySelector('#yearB')
-        yearB.innerHTML = book.year
-        let avB = document.querySelector('#avB')
-        avB.innerHTML = book.availability
-        let revB = document.querySelector('#revB')
-        revB.innerHTML = book.review
+        for (let book of books) {
+            book = new Book(book.title, book.author, book.year, book.review, book.sharedBy, book.availability, book.id)
+            let bookTitle = document.createElement('a')
+            bookTitle.innerHTML = book.title
+            bookSection.append(bookTitle)
+            bookTitle.addEventListener('click', function () {
+                console.log(book.id)
+                console.log(book)
+                let titleB = document.querySelector('#titleB')
+                titleB.innerHTML = book.title
+                let authorB = document.querySelector('#authorB')
+                authorB.innerHTML = book.author
+                let yearB = document.querySelector('#yearB')
+                yearB.innerHTML = book.year
+                let avB = document.querySelector('#avB')
+                avB.innerHTML = book.availability
+                let revB = document.querySelector('#revB')
+                revB.innerHTML = book.review
+                let pb = document.querySelectorAll('.pb')
+
+
+                for (let p of pb){
+                if (book.availability == 'No') {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Someone got it first!',
+                    })
+                    
+                        p.style.color = 'lightgrey'
+                
+
+                } else {
+                        p.style.color = 'black'
+                   
+                }
+                }
+
+            })
+            let delBtn = document.querySelector('#delB')
+                delBtn.addEventListener('click', function(){
+                    console.log(book)
+                    deleteBook(book.id, bookTitle)
+                    for(let p of pb){
+                        p.innerHTML=''
+                    }
+                })
+           
+        }
     })
-
-    if(book.availability == 'No'){
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'Someone got it first!',
-          })
-        titleB.style.color = 'lightgrey'
-        authorB.style.color = 'lightgrey'
-        yearB.style.color = 'lightgrey'
-        avB.style.color = 'lightgrey'
-        revB.style.color = 'lightgrey'
-    }
-    
-    }
-})
 
 
 let addUser = document.querySelector('#addUser')
-    addUser.addEventListener('click', function(){
-        location.href = 'createUser.html'
-    })
+addUser.addEventListener('click', function () {
+    location.href = 'createUser.html'
+})
 
 let addBook = document.querySelector('#addBook')
-    addBook.addEventListener('click', function(){
-        location.href = 'createBook.html'
-    })
+addBook.addEventListener('click', function () {
+    location.href = 'createBook.html'
+})
